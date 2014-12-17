9a1a1af12d72c1665dcd5221bf80e7b59f0a0d2c
Francis Chong <francis@ignition.hk>
2014-12-18 00:11:22 +0800

    add many events

--- test/GameEventHandlersTest.js
+++ test/GameEventHandlersTest.js
@@ -87,15 +87,66 @@ describe('GameEventHandlers', function(){
     })
   })
 
-  describe("#card_attached()", function(){
+  describe("#attached()", function(){
     it('should add card to user deck', function(){
         GameEventHandlers.open_card(game, {id: 69, card_id: "FP1_028e"});
-        GameEventHandlers.open_card(game, {id: 57, card_id: "FP1_028e"});
-        GameEventHandlers.card_attached(game, {id: 69, target: 57});
+        GameEventHandlers.open_card(game, {id: 57, card_id: "FP1_028"});
+        GameEventHandlers.attached(game, {id: 69, target: 57});
 
         var entity = game.getEntityWithId(69);
         var target = game.getEntityWithId(57);
         expect(target.attachments).to.contain(entity)
     })
   })
+
+  describe("#damage()", function(){
+    it('should set damage on target', function(){
+        GameEventHandlers.open_card(game, {id: 57, card_id: "FP1_028"});
+        GameEventHandlers.damage(game, {id: 57, amount: 2});
+
+        var target = game.getEntityWithId(57);
+        expect(target.damaged).to.equal(2);
+    });
+  })
+
+  describe("#card_played()", function(){
+    it('should moved to play', function(){
+        GameEventHandlers.open_card(game, {id: 57, card_id: "CS2_101"});
+        GameEventHandlers.card_played(game, {id: 57, player_id: 2, card_id: "CS2_101"});
+
+        var entity = game.getEntityWithId(57);
+        expect(entity.player).equal(game.getPlayerWithId(2));
+        expect(entity.zone).equal(ZONES.PLAY);
+    })
+  })
+
+  describe("#card_discarded()", function(){
+    it('should moved to graveyard', function(){
+        GameEventHandlers.open_card(game, {id: 57, card_id: "CS2_101"});
+        GameEventHandlers.card_played(game, {id: 57, player_id: 2, card_id: "CS2_101"});
+        GameEventHandlers.card_discarded(game, {id: 57, player_id: 2, card_id: "CS2_101"});
+        var entity = game.getEntityWithId(57);
+        expect(entity.zone).equal(ZONES.GRAVEYARD);
+    })
+  })
+
+  describe("#card_destroyed()", function(){
+    it('should moved to graveyard', function(){
+        GameEventHandlers.open_card(game, {id: 57, card_id: "CS2_101"});
+        GameEventHandlers.card_played(game, {id: 57, player_id: 2, card_id: "CS2_101"});
+        GameEventHandlers.card_destroyed(game, {id: 57, player_id: 2, card_id: "CS2_101"});
+        var entity = game.getEntityWithId(57);
+        expect(entity.zone).equal(ZONES.GRAVEYARD);
+    })
+  })
+
+  describe("#card_put_in_play()", function(){
+    it('should added to play', function(){
+        GameEventHandlers.card_put_in_play(game, {id: 57, player_id: 2, card_id: "CS2_101"});
+        var entity = game.getEntityWithId(57);
+        expect(entity.card.id).equal("CS2_101");
+        expect(entity.zone).equal(ZONES.PLAY);
+    })
+  })
+  
 })
